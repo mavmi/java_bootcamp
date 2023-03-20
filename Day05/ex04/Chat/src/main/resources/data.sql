7efce5d81dd84f3a96fcc46616455f62aabcecf6
username <matveev.ivmi@yandex.ru>
2023-03-21 01:08:02 +0300

    d05 e04: sql statement

--- Day05/ex04/Chat/src/main/resources/data.sql
+++ Day05/ex04/Chat/src/main/resources/data.sql
@@ -5,17 +5,6 @@ insert into "user"(login, password) values('username4', 'passwd4');
 insert into "user"(login, password) values('username5', 'passwd5');
 insert into "user"(login, password) values('username6', 'passwd6');
 insert into "user"(login, password) values('username7', 'passwd7');
-insert into "user"(login, password) values('username8', 'passwd8');
-insert into "user"(login, password) values('username9', 'passwd9');
-insert into "user"(login, password) values('username10', 'passwd10');
-insert into "user"(login, password) values('username11', 'passwd11');
-insert into "user"(login, password) values('username12', 'passwd12');
-insert into "user"(login, password) values('username13', 'passwd13');
-insert into "user"(login, password) values('username14', 'passwd14');
-insert into "user"(login, password) values('username15', 'passwd15');
-insert into "user"(login, password) values('username16', 'passwd16');
-insert into "user"(login, password) values('username17', 'passwd17');
-insert into "user"(login, password) values('username18', 'passwd18');
 
 
 insert into chatroom(name, owner) values('chatroom1', 1);
@@ -23,9 +12,17 @@ insert into chatroom(name, owner) values('chatroom2', 2);
 insert into chatroom(name, owner) values('chatroom3', 3);
 insert into chatroom(name, owner) values('chatroom4', 4);
 insert into chatroom(name, owner) values('chatroom5', 5);
+insert into chatroom(name, owner) values('chatroom6', 1);
+insert into chatroom(name, owner) values('chatroom7', 7);
 
 insert into "message"(author, room, text, datetime) values(1, 1, 'message1', '2023-01-01 12:12:12');
 insert into "message"(author, room, text, datetime) values(2, 2, 'message2', '2023-01-02 12:12:12');
 insert into "message"(author, room, text, datetime) values(3, 3, 'message3', '2023-01-03 12:12:12');
 insert into "message"(author, room, text, datetime) values(4, 4, 'message4', '2023-01-04 12:12:12');
 insert into "message"(author, room, text, datetime) values(5, 5, 'message5', '2023-01-05 12:12:12');
+insert into "message"(author, room, text, datetime) values(1, 5, 'message6', '2023-01-06 12:12:12');
+insert into "message"(author, room, text, datetime) values(2, 4, 'message7', '2023-01-07 12:12:12');
+insert into "message"(author, room, text, datetime) values(3, 3, 'message8', '2023-01-08 12:12:12');
+insert into "message"(author, room, text, datetime) values(4, 2, 'message9', '2023-01-09 12:12:12');
+insert into "message"(author, room, text, datetime) values(5, 1, 'message10', '2023-01-10 12:12:12');
+insert into "message"(author, room, text, datetime) values(6, 1, 'message11', '2023-01-11 12:12:12');
