85c07faab602b1ae70c09405b2da5b9fa4561e32
Plums Maryjo <pmaryjo@at-j5.msk.21-school.ru>
2023-03-24 20:41:37 +0300

    minor

--- Day08/ex00/Spring/src/main/java/edu/school21/app/Program.java
+++ Day08/ex00/Spring/src/main/java/edu/school21/app/Program.java
@@ -10,25 +10,25 @@ public class Program {
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         {
-            Printer printer = context.getBean("printerWithPrefix", Printer.class);
-            printer.print("Hello!");
-        }
-        {
-            Printer printer = context.getBean("printerWithDateTime", Printer.class);
-            printer.print("Hello!");
-        }
-        {
             PreProcessor preProcessor = new PreProcessorToLowerImpl();
             Renderer renderer = new RendererStandardImpl(preProcessor);
             PrinterWithDateTimeImpl printer = new PrinterWithDateTimeImpl(renderer);
-            printer.print("Hello!");
+            printer.print("1a");
         }
         {
             PreProcessor preProcessor = new PreProcessorToUpperImpl();
             Renderer renderer = new RendererErrImpl(preProcessor);
             PrinterWithPrefixImpl printer = new PrinterWithPrefixImpl(renderer);
-            printer.setPrefix("Prefix");
-            printer.print("Hello!");
+            printer.setPrefix("Prefixxx");
+            printer.print("22aA");
+        }
+        {
+            Printer printer = context.getBean("printerDateTimeStdToLower", Printer.class);
+            printer.print("333aAa");
+        }
+        {
+            Printer printer = context.getBean("printerDateTimeErrToUpper", Printer.class);
+            printer.print("4444aAAa");
         }
     }
 }
